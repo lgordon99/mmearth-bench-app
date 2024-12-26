59837a1ec1b969368c14e0cfa1084c0e5215af96
lgordon99 <luciagordon@g.harvard.edu>
2024-12-26 15:01:36 -0500

    Only loaded visible tiles and added all modalities

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let map = L.map('map').setView([0, 0], 2);
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
 let Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}?api_key=a43934c7-f6fc-4a3d-9165-e19550683b0d', {
 	minZoom: 0,
 	maxZoom: 20,
@@ -7,34 +6,89 @@ let Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_brigh
 	ext: 'png'
 });
 
-// const tasks = ['biomass', 'species', 'soil_nitrogen', 'soil_organic_carbon', 'soil_pH'];
 const tasks = {
-	// 'species': {'title': 'Species', 'color': 'red'},
-               'soil_nitrogen': {'title': 'Soil nitrogen', 'color': 'blue'}};
+			//    'species': {'title': 'Species', 'color': 'red'},
+               'soil_nitrogen': {'title': 'Soil nitrogen', 'color': 'blue'},
+			//    'soil_organic_carbon': {'title': 'Soil organic carbon', 'color': 'brown'},
+			//    'soil_pH': {'title': 'Soil pH', 'color': 'purple'}
+			};
 let layers = Object.fromEntries(Object.keys(tasks).map(task => [task, {'tileLayer': L.layerGroup(),
-        												  				'toolTipLayer': L.layerGroup(),
-        												  				'sentinel2Layer': L.layerGroup()}]));
-let soilNitrogenLayer = L.layerGroup();
-let soilNitrogenToolTipLayer = L.layerGroup();
-let sentinel2Layer = L.layerGroup();
+        												  			   'toolTipLayer': L.layerGroup(),
+        												  			   'Sentinel-2': {},
+																	   'Sentinel-1': {},
+																	   'AsterDEM-elevation': {},
+																	   'ETHGCH-canopy-height': {},
+																	   'DynamicWorld': {},
+																	   'ESA-Worldcover': {}
+																	}]));
+const pixelLevelModalities = ['Sentinel-2','Sentinel-1', 'AsterDEM-elevation', 'ETHGCH-canopy-height', 'DynamicWorld', 'ESA-Worldcover']
+const hoverPanel = document.getElementById('hover-panel');
+const taskValue = document.getElementById('task-value')
+const imageLevelModalities = document.getElementById('image-level-modalities-data');
+const imageLevelModalityCheckbox = document.getElementById('image-level-modalities-checkbox');
+let activeBounds = null;
+let selectedModality = null;
 
-Stadia_OSMBright.addTo(map);
+// Add event listener for the checkbox to toggle modalities info
+imageLevelModalityCheckbox.addEventListener('change', function () {
+    if (imageLevelModalityCheckbox.checked) {
+        imageLevelModalities.style.display = 'block';
+    } else {
+        imageLevelModalities.style.display = 'none';
+    }
+});
 
-// L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
-//     subdomains: 'abcd',
-//     maxZoom: 19
-// }).addTo(map);
-// L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a> contributors',
-//     maxZoom: 18
-// }).addTo(map);
+function round(num, decimals) {
+    const factor = Math.pow(10, decimals);
+    return Math.round(num * factor) / factor;
+}
+
+function showHoverPanel(e, taskData, imageLevelModalityData) {
+	const layer = e.target;
+    const bounds = layer.getBounds();
+    const topLeft = bounds.getNorthWest();
+    const point = map.latLngToContainerPoint(topLeft); // converts the geographical coordinates to pixel coordinates
+
+    hoverPanel.style.display = 'block';
+    hoverPanel.style.left = `${point.x}px`;
+    hoverPanel.style.top = `${point.y}px`;
+	taskValue.innerText = taskData;
+	imageLevelModalities.innerText = imageLevelModalityData;
+}
+
+function hideHoverPanel() {
+    hoverPanel.style.display = 'none';
+}
+
+map.on('mousemove', function(e) {
+    if (activeBounds) {
+        // check if mouse is within the bounds of the tile last hovered over
+        if (!activeBounds.contains(e.latlng)) {
+            hideHoverPanel();
+            activeBounds = null;
+        }
+    }
+});
+
+map.on('zoomend', function(e) {
+	if (activeBounds) {
+		const topLeft = activeBounds.getNorthWest();
+		const point = map.latLngToContainerPoint(topLeft);
+		hoverPanel.style.left = `${point.x}px`;
+		hoverPanel.style.top = `${point.y}px`;
+	}
+
+	showVisibleTiles(selectedModality);
+})
+
+map.on('moveend', () => showVisibleTiles(selectedModality));
 
 function showTilesWithoutModality() {
 	for (const task of Object.keys(tasks)) {
 		fetch(`${task}/${task}_tile_gdf.geojson`)
 		.then(response => response.json())
 		.then(data => {
+			// solid tile
 			L.geoJson(data, {
 				style: function(feature) {
 					return {
@@ -45,19 +99,21 @@ function showTilesWithoutModality() {
 					};
 				}
 			}).addTo(layers[task]['tileLayer']);
-	
+			
+			// tile border
 			L.geoJson(data, {
 				style: function(feature) {
 					return {
 						fillOpacity: 0,
 						weight: 3,
 						color: tasks[task]['color'],
-						interactive: false,  // Border shouldn't capture interactions
-						pane: 'borderPane'  // Lower pane for borders
+						interactive: false,  // border shouldn't capture interactions
+						pane: 'borderPane'  // lower pane for borders
 					};
 				}
 			}).addTo(layers[task]['toolTipLayer']);
 	
+			// task value upon hover
 			L.geoJson(data, {
 				style: function(feature) {
 					return {
@@ -68,7 +124,29 @@ function showTilesWithoutModality() {
 					};
 				},
 				onEachFeature: function (feature, layer) {
-					layer.bindTooltip(`${tasks[task]['title']}: ${feature.properties[task]}`);
+					// layer.bindTooltip(`${tasks[task]['title']}: ${feature.properties[task]}`);
+					layer.on({
+						mouseover: function (e) {
+							activeBounds = layer.getBounds();
+							let taskData = `${tasks[task]['title']}: ${feature.properties[task]}`;
+						
+							if (task == 'soil_nitrogen' || task == 'soil_organic_carbon') {
+								taskData += ' g/kg';
+							}
+
+							const imageLevelModalityData = `Latitude: ${feature.properties['latitude']}
+							Longitude: ${feature.properties['longitude']}
+							Month: ${feature.properties['month']}
+							Biome: ${feature.properties['biome']}
+							Ecoregion: ${feature.properties['ecoregion']}\n
+							Climate
+							${Object.entries(feature.properties['climate'])
+								.map(([key, value]) => `${key}: ${round(value, 2)} ${key.includes('Temperature') ? 'K' : 'm'}`)
+								.join('\n')}`
+								.trim();
+							showHoverPanel(e, taskData, imageLevelModalityData);
+						}
+					});
 				}
 			}).addTo(layers[task]['toolTipLayer']);
 		
@@ -78,38 +156,85 @@ function showTilesWithoutModality() {
 	}
 }
 
-function loadSentinel2() {
+// function loadPixelLevelModalities() {
+// 	for (const task of Object.keys(tasks)) {
+// 		fetch(`${task}/${task}_tile_bounds.json`)
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			for (const key in data) {
+// 				for (const modality of pixelLevelModalities) {
+// 					const modalityLayer = L.imageOverlay(`${task}/tiles/${modality}/${key}${modality}.png`,
+// 														 data[key],
+// 														 {opacity: 0.9});
+// 					layers[task][modality].addLayer(modalityLayer);
+// 				}
+// 			}
+// 		});
+// 	}
+// }
+
+function loadPixelLevelModalities() {
 	for (const task of Object.keys(tasks)) {
-		fetch(`${task}/${task}_Sentinel-2_tile_bounds.json`)
+		fetch(`${task}/${task}_tile_bounds.json`) // fetches the tile bounds JSON for the given task
 		.then(response => response.json())
 		.then(data => {
 			for (const key in data) {
-				const tileLayer = L.imageOverlay(`${task}/${task}_tiles/Sentinel-2/${key}Sentinel-2.png`, data[key], {
-					opacity: 0.7
-				});
-				layers[task]['sentinel2Layer'].addLayer(tileLayer);
+				const bounds = L.latLngBounds([[data[key][0][0], data[key][0][1]], // southwest corner
+											[data[key][1][0], data[key][1][1]]]); // northeast corner
+					for (const modality of pixelLevelModalities) {
+						const tileLayer = L.imageOverlay(`${task}/tiles/${modality}/${key}${modality}.png`,
+															data[key],
+															{opacity: 0.9});
+						layers[task][modality][key] = {layer: tileLayer, bounds: bounds};
+					}
+				}
 			}
-			layers[task]['sentinel2Layer'].addTo(map);
-			map.removeLayer(layers[task]['sentinel2Layer']);
-		});
+		);
 	}
 }
 
+function showVisibleTiles(modality) {
+	const visibleBounds = map.getBounds(); // gets the current map bounds
+
+	for (const task of Object.keys(tasks)) {
+		if (document.getElementById(`${task}-checkbox`).checked) {
+			for (const key in layers[task][modality]) {
+				const tile = layers[task][modality][key];
+
+				if (visibleBounds.intersects(tile.bounds)) {
+					if (!map.hasLayer(tile.layer)) {
+						map.addLayer(tile.layer);
+					}
+					for (const otherModality of pixelLevelModalities) {
+						if (otherModality != modality) {
+							const otherTile = layers[task][otherModality][key];
+
+							if (map.hasLayer(otherTile.layer)) {
+								map.removeLayer(otherTile.layer);
+							}	
+						}
+					}
+				} else {
+					if (map.hasLayer(tile.layer)) {
+						map.removeLayer(tile.layer);
+					}
+				}
+			}
+		}
+	}
+}
+
+Stadia_OSMBright.addTo(map);
+
 map.createPane('borderPane');
-map.getPane('borderPane').style.zIndex = 400;  // Lower z-index for borders
+map.getPane('borderPane').style.zIndex = 400; // lower z-index for borders
 
 map.createPane('tooltipPane');
-map.getPane('tooltipPane').style.zIndex = 650;  // Higher z-index for tooltips
+map.getPane('tooltipPane').style.zIndex = 650; // higher z-index for tooltips
 map.getPane('tooltipPane').style.pointerEvents = 'all';
 
 showTilesWithoutModality();
-loadSentinel2()
-// var tileLayer = L.tileLayer('http://localhost:8000/soil_nitrogen/rgb/{z}/{x}/{y}.png', {
-// 		maxZoom: 10,    // Maximum zoom level from gdal2tiles
-// 		attribution: '&copy; Your Tile Attribution'
-// 	});
-
-// 	tileLayer.addTo(map);
+loadPixelLevelModalities();
 
 document.getElementById("world-map").addEventListener("click", function() {
 	Stadia_OSMBright.addTo(map);
@@ -119,6 +244,7 @@ document.getElementById("clear").addEventListener("click", function() {
 	Stadia_OSMBright.remove();
 });
 
+// task buttons clicked
 for (const task of Object.keys(tasks)) {
 	document.getElementById(`${task}-checkbox`).addEventListener("change", function(e) {
 		if (e.target.checked) { // if task checkbox checked
@@ -128,8 +254,10 @@ for (const task of Object.keys(tasks)) {
 				map.addLayer(layers[task]['tileLayer']);
 			}
 
-			if (document.getElementById("sentinel-2").checked) {
-				map.addLayer(layers[task]['sentinel2Layer']);
+			for (const modality of pixelLevelModalities) {
+				if (document.getElementById(modality).checked) {
+					map.addLayer(layers[task][modality]);
+				}
 			}
 		} else { // if task checkbox unchecked
 			map.removeLayer(layers[task]['toolTipLayer']);
@@ -138,28 +266,39 @@ for (const task of Object.keys(tasks)) {
 				map.removeLayer(layers[task]['tileLayer']);
 			}	
 
-			if (map.hasLayer(layers[task]['sentinel2Layer'])) {
-				map.removeLayer(layers[task]['sentinel2Layer']);
-			}	
+			for (const modality of pixelLevelModalities) {
+				if (map.hasLayer(layers[task][modality])) {
+					map.removeLayer(layers[task][modality]);
+				}
+			}
 		}
 	});
 }
 
+// None button clicked
 document.getElementById("none").addEventListener("click", function() {
+	selectedModality = null;
+
 	for (const task of Object.keys(tasks)) {
 		if (document.getElementById(`${task}-checkbox`).checked) {
 			map.addLayer(layers[task]['tileLayer']);
 		};
 
-		if (map.hasLayer(layers[task]['sentinel2Layer'])) {
-			map.removeLayer(layers[task]['sentinel2Layer']);
+		for (const modality of pixelLevelModalities) {
+			if (map.hasLayer(layers[task][modality])) {
+				map.removeLayer(layers[task][modality]);
+			}
 		}
 	}
 });
 
-document.getElementById("sentinel-2").addEventListener("click", function() {
-	for (const task of Object.keys(tasks)) {
-		map.removeLayer(layers[task]['tileLayer']);
-		map.addLayer(layers[task]['sentinel2Layer']);	
-	}
-});
+// modality buttons clicked
+for (const modality of pixelLevelModalities) {
+	document.getElementById(modality).addEventListener("click", function() {
+		for (const task of Object.keys(tasks)) {
+			map.removeLayer(layers[task]['tileLayer']);
+			showVisibleTiles(modality);
+			selectedModality = modality;
+		}
+	});
+}
